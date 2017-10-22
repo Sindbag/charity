b9c5379cda845ad50f5e4ffbfa1b5d4829de6ddd
anatoly <sindbag@gmail.com>
2017-10-22 06:25:26 +0300

    Rotating world added

--- front/js/d3-custom-code2.js
+++ front/js/d3-custom-code2.js
@@ -1,97 +1,233 @@
-'use strict';
-
-const svg = d3.select("#svg-container");
+let svg = d3.select("#svg-container");
 let width = svg.node().getBoundingClientRect().width,
     height = svg.node().getBoundingClientRect().height;
 
-var places = {
-  HNL: [-157 - 55 / 60 - 21 / 3600, 21 + 19 / 60 + 7 / 3600],
-  HKG: [113 + 54 / 60 + 53 / 3600, 22 + 18 / 60 + 32 / 3600],
-  SVO: [37 + 24 / 60 + 53 / 3600, 55 + 58 / 60 + 22 / 3600],
-  HAV: [-82 - 24 / 60 - 33 / 3600, 22 + 59 / 60 + 21 / 3600],
-  CCS: [-66 - 59 / 60 - 26 / 3600, 10 + 36 / 60 + 11 / 3600],
-  UIO: [-78 - 21 / 60 - 31 / 3600, 0 + 6 / 60 + 48 / 3600]
-};
+svg = d3.select("#svg-container")
+    .attr("width", width)
+    .attr("height", height);
 
-var route = {
-  type: "LineString",
-  coordinates: [
-    places.HNL,
-    places.HKG,
-    places.SVO,
-    places.HAV,
-    places.CCS,
-    places.UIO
-  ]
-};
+let geojson, projection;
 
-var projection = d3.geo.kavrayskiy7()
-    .scale(170)
-    .rotate([-40, 0])
-    .translate([width / 2, height / 2])
-    .precision(.1);
+let tile = d3.tile()
+    .size([width, height]);
 
-var path = d3.geo.path()
+const pi = Math.PI,
+    tau = 2 * pi;
+
+// Update projection
+projection = d3.geoOrthographic()
+    .scale(1 / tau)
+    .translate([0, 0]);
+
+const geoGenerator = d3.geoPath()
     .projection(projection);
 
-var graticule = d3.geo.graticule();
+const graticule = d3.geoGraticule();
+const color = d3.scaleOrdinal(d3.schemeCategory20);
+const geoCircle = d3.geoCircle().radius(0.3).precision(1);
 
-svg.append("defs").append("path")
-    .datum({type: "Sphere"})
-    .attr("id", "sphere")
-    .attr("d", path);
+const zoom = d3.zoom()
+    .scaleExtent([1 << 11, 1 << 14])
+    // .translateExtent([[-100, -100], [width + 90, height + 100]])
+    .on("zoom", zoomed);
 
-svg.append("use")
-    .attr("class", "stroke")
-    .attr("xlink:href", "#sphere");
+const maps = d3.select('g.map');
+const circles = d3.select('.circles');
+const lines = d3.select('.lines');
+const grat = d3.select('.graticule');
 
-svg.append("use")
-    .attr("class", "fill")
-    .attr("xlink:href", "#sphere");
+geoGenerator.projection(projection);
 
-svg.append("path")
-    .datum(graticule)
-    .attr("class", "graticule")
-    .attr("d", path);
+function update(cities, connects) {
+  // Update world map
+  let u = maps
+    .selectAll('path')
+    .data(geojson.features);
 
-svg.append("path")
-    .datum(route)
-    .attr("class", "route")
-    .attr("d", path);
+  u.enter()
+    .append('path')
+    .merge(u)
+    .attr('d', geoGenerator);
 
-var point = svg.append("g")
-    .attr("class", "points")
-  .selectAll("g")
-    .data(d3.entries(places))
-  .enter().append("g")
-    .attr("transform", function(d) { return "translate(" + projection(d.value) + ")"; });
+  // Update graticule
+  grat
+    .datum(graticule())
+    .attr('d', geoGenerator);
 
-point.append("circle")
-    .attr("r", 4.5);
+  // update circles
+  u = circles
+    .selectAll('path')
+    .data(cities.map(function(d) {
+          geoCircle.center([d['lng'] || d.lon, d.lat]).radius((d.rad || 7000) / 30000);
+          let tmp = geoCircle();
+          tmp['group'] = d.group + 1;
+          tmp['name'] = d.city;
+          return tmp;
+        })
+    );
 
-point.append("text")
-    .attr("y", 10)
-    .attr("dy", ".71em")
-    .text(function(d) { return d.key; });
+  let up = u.enter()
+    .append('path')
+    .attr('opacity', 0);
+  up
+    .transition().duration(600)
+    .attr("opacity", 1);
+  up
+    .merge(u)
+    .attr('d', geoGenerator)
+    .attr("fill", function(d) { return color(d.group); })
+    .attr('data-toggle', 'modal')
+    .attr('data-target', '#charityModal')
+    .attr('data-name', d => d.name)
+    .attr('data-count', d => +d.group)
+    .append("svg:title")
+    .text(function(d, i) { return d.name});
 
-const err = d3.select('#error-message');
+  // update lines
+    if (connects.length) {
+        u = lines
+            .selectAll('path')
+            .data(connects);
 
-d3.json("/charity/front/data/world-50m.json", function(error, world) {
-  if (error) {
-      console.log(error);
-      err.append('p').text(error);
-      throw error;
+        up = u.enter()
+            .append('path');
+        up
+            .interrupt().transition()
+            .duration(1000).ease(d3.easeLinear);
+        up
+            .merge(u)
+            .attr("d", geoGenerator)
+            .attr('stroke', function (d) {
+                return color(+d.group);
+            });
+    }
+  // Compute the projected initial center.
+  let center = projection([37.5, 55.4]);
+  //
+  // Apply a zoom transform equivalent to projection.{scale,translate,center}.
+  if (!called) {
+      svg
+          .call(zoom)
+          .call(zoom.transform, d3.zoomIdentity
+              .translate(width / 2, height / 2)
+              .scale(1 << 12)
+              .translate(-center[0], -center[1]));
+      called = true;
   }
+}
 
-  svg.insert("path", ".graticule")
-      .datum(topojson.feature(world, world.objects.land))
-      .attr("class", "land")
-      .attr("d", path);
+let called = false;
 
-  svg.insert("path", ".graticule")
-      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-      .attr("class", "boundary")
-      .attr("d", path);
+
+d3.json('/charity/front/data/bigc.json', function (err, json2) {
+    if (err) throw err;
+    let cities = json2;
+
+    d3.json('/charity/front/data/bigl.json', function (err, links) {
+        if (err) throw err;
+        let connects = links;
+        d3.json('/charity/front/data/world.geo.json', function(err, json3) {
+            if (err) throw err;
+            geojson = json3;
+            perform_updates(cities, connects);
+        });
+    });
 });
 
-d3.select(self.frameElement).style("height", height + "px");
+function perform_updates(cts, cnts) {
+    let used = [],
+        used_ct = [],
+        used_cn = [];
+    i = 0;
+    cnts = cnts.map(function (d) {
+        return {
+            group: d.group,
+            source: d.source,
+            target: d.target,
+            type: 'Feature', geometry: {
+                type: 'LineString',
+                coordinates:  [
+                    d.coords[1].reverse(),
+                    d.coords[0].reverse()
+                ]
+            }
+        };
+    });
+
+    function fun () {
+        if (i >= cts.length) return -1;
+
+        ct = cts[i];
+
+        used.push(ct['city']);
+        used_ct.push(ct);
+        let tmp = cnts.filter(cn => (used.indexOf(cn.source) > -1
+            && used.indexOf(cn.target) > -1
+            && used_cn.indexOf(cn) === -1));
+
+        used_cn = used_cn.concat(tmp);
+        update(used_ct, used_cn);
+        i += 1;
+
+        setTimeout(() => fun(), 100);
+    }
+    fun();
+}
+
+setInterval(rotate, 100);
+
+let yaw = 300;
+function rotate() {
+  projection.rotate([yaw, -45]);
+  yaw -= 1;
+
+  circles.selectAll('path')
+      .attr("d", geoGenerator);
+  lines.selectAll('path')
+      .attr("d", geoGenerator);
+  grat.select('path')
+      .attr("d", geoGenerator);
+  maps.selectAll('path')
+      .attr("d", geoGenerator);
+
+}
+
+function zoomed() {
+  const transform = d3.event.transform;
+  const tiles = tile
+      .scale(transform.k)
+      .translate([transform.x, transform.y])
+      ();
+
+  projection
+      .scale(transform.k / tau)
+      .translate([transform.x, transform.y]);
+
+  circles.selectAll('path')
+      .attr("d", geoGenerator);
+  lines.selectAll('path')
+      .attr("d", geoGenerator);
+  grat.select('path')
+      .attr("d", geoGenerator);
+  maps.selectAll('path')
+      .attr("d", geoGenerator);
+
+}
+
+
+function stringify(scale, translate) {
+  let k = scale / 256, r = scale % 1 ? Number : Math.round;
+  return "translate(" + r(translate[0] * scale) + "," + r(translate[1] * scale) + ") scale(" + k + ")";
+}
+
+
+$('#charityModal').on('show.bs.modal', function (event) {
+  let button = $(event.relatedTarget); // Button that triggered the modal
+  let city = button.data('name'); // Extract info from data-* attributes
+  let count = button.data('count'); // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  let modal = $(this);
+  modal.find('.modal-title').text('Благотворительность в городе ' + city);
+  modal.find('#modalData').html('Ого: ' + count)
+});
